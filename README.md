77aced5f2ea7e7c86dbab2add274e78b30a5f626
André Diego Piske <andrepiske@gmail.com>
2013-02-03 16:42:00 -0200

    Added 'exec' function

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 
 lua OS Layer
 
+Execute processes in lua with bidirectional pipes for stdin and stdout (no stderr yet). Just because io.popen() is not bidirectional.
+
+Linux only, but I plan to port this to Windows someday. 
+
+
+Issues & feature request: "Issues" page on github.
+
+
 Compile & Install
 =============
 
@@ -18,9 +26,113 @@ You may change that to work with lua5.2 (also change the compile.sh to link to l
 Usage
 =====
 
-This lib has 1 function:
+This lib has 2 functions:
+
+    exec(command, {arguments})
 
     exec_n_pipe(pipe_stderr, command, command_name, ...)
+
+*exec*
+--------
+This will execute the program *command* with *arguments* and will pipe its stdout, stderr and stdin.
+
+Note that arguments[1] will be the command`s argv[0] and so on.
+
+**Return Value**
+
+The return value is an userdata with the following functions:
+
+    out([len; default=1024])
+    put(content, [offset], [len])
+    close()
+    waitend()
+    getpipesize()
+
+* **out()** will read the program stdout up to *len* bytes and will return two values. The first is how many bytes has been read and the second is the bytes read (a string). The first return value may be zero or lower. When zero, end-of-file has been reached. When negative, an error occured.
+
+* **put()** will write *content* to stdin. The methods A and B in the example work exactly the same, although method A is faster.
+
+        local proc = oslayr.exec('cat', {'cat'}) 
+        local content = ...
+        ... some very important code here ...
+        proc:put(content, 10, 100) -- method A
+        proc:put(string.gsub(content, 10, 110)) -- method B
+
+* **close()** Closes the open program. It is very important that you call this after reading and writing everything. This method will free resources (buffers and file descriptors). It is safe to call waitend() after close() has been called. **NOTE** that this function will NOT kill the program, but will close its stdout, stdin and stderr, which may kill it via a unhandled SIGPIPE if the program tries to write to a broken pipe. **NOTE 2**  no that this won't be called automatically on garbage collection, so you **must** call this when appropriate.
+
+* **waitend()** This will wait for the program end and will return its exit status. It will only return when the program ends. No implicit (nor explicit) timeouts here. This is the only function that is safe to be called after close().
+
+* **getpipesize()** Returns three values (integers): The size (in bytes, in this order) of the stdin, stdout and stderr pipes. See caveats section below.
+
+**Examples**
+
+Example 1. Shows the waitend() funcionality:
+
+    require('oslayr')
+    local f = oslayr.exec('false', {'false'})
+    print(f:waitend())
+
+Ouput:
+
+    1
+
+Example 2. Shows interactive usage:
+
+    require('oslayr')
+    local f = oslayr.exec('cat', {'cat', '-e'})
+    f:put('Hel')
+    f:put('lo')
+    local l, x = f:out()
+    print(x)
+    f:put('\n')
+    f:out() -- discard it
+    f:put('foo\n')
+    l, x = f:out()
+    print(l)
+    print(x)
+
+Output:
+
+    Hello
+    5
+    foo$
+
+There are no buffers in put() function, so there is no need of "\n" to flush buffers (there are no write buffers!).
+
+The length printed (the "5" you see there) are those 5 chars: { 'f', 'o', 'o', '$', '\n' }.
+
+The '$' appears because of the '-e' argument (See cat(1)).
+
+**Caveats**
+
+Pipes are buffered by the kernel, but their buffer size is not unlimited. A value of 65536 bytes may be assumed for Linux and OS X, but the getpipesize() function returns the exact value.
+
+When one tries to write to a pipe that is full, the put() function will block until the pipe gets free room, which happens when the other end of the pipe reads data from it.
+
+The out() function blocks when there is no data available.
+
+Here is an example of an application that will wait forever due to unlimited pipe buffers and blocks:
+
+    require('oslayr')
+    local f = oslayr.exec('cat', {'cat'})
+    f:put(string.rep('.', 1024 * 500)) -- 500kB string
+
+This program will block and won't ever continue for a pipe buffer size of 65kB and some theoretical implementation of the cat program. Here is what might happen (I'll refer "us" as this lua program and "them" as the cat program):
+
+1. We write 500kB to the stdin pipe
+2. They read 65kB from the stdin pipe
+3. They write this same 65kB to their stdout pipe, which is what we get when we call f:out()
+4. Now their stdout pipe is filled with 65kB
+5. They read, let's say, 1 byte from their stdin.
+6. They write this byte to their stdout. This write will block, as their stdout pipe is full.
+
+To unblock their stdout pipe, we must f:out(), but we also are blocked. Dead End.
+
+Question: how to overcome this? Answer: do not.
+A buffered implementation of this very lib is planned to be written, some code is done, but time is needed and a lot of it is in this documentation.
+
+*exec\_n\_pipe* 
+---------------
 
 This will execute the program *command*. *command\_name* is what the program will receive in argv[0].
 Usually you'll want to use the same value for *command* and *command\_name*
@@ -49,6 +161,6 @@ Output:
     opt
     root
     
-I continues, but you got the idea.
+It continues, but you got the idea.
 
 
--- oslayr.c
+++ oslayr.c
@@ -5,13 +5,296 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <lua.h>
 #include <lauxlib.h>
 #include <errno.h>
+#include <signal.h>
+// #include <pthread.h>
 
-int _f_exec_n_pipe(lua_State *L) {
+#define OSLAYR_EXECTYPE "oslayr_exec"
+
+struct s_exec {
+    pid_t pid;
+    int pin[2];
+    int pout[2];
+    int perr[2];
+    void *bufferout;
+    size_t bosize; // , boused;
+    // int do_bufferout;
+    // pthread_mutex *bo_mx;
+};
+
+static struct s_exec*
+_get_exec(lua_State *L) {
+    return (struct s_exec*)luaL_checkudata(L, 1, OSLAYR_EXECTYPE);
+}
+
+static void
+_exec_assert_bs(struct s_exec *se, size_t amount) {
+    if (se->bosize >= amount)
+        return;
+    se->bosize = amount;
+    if (se->bufferout)
+        free(se->bufferout);
+    se->bufferout = malloc(amount);
+}
+
+/*
+static int 
+_exec_read_with_bufferout(lua_State *L, struct s_exec *se, size_t amount) {
+    size_t maxread;
+    pthread_mutex_lock(se->bo_mx);
+    maxread = 
+    pthread_mutex_unlock(se->bo_mx);
+    return 0;
+}
+*/
+
+static int
+_f_read_out(lua_State *L) {
+    struct s_exec *se = _get_exec(L);
+    size_t amount = 1024;
+    ssize_t r;
+
+    if (lua_isnumber(L, 2))
+        amount = (size_t)lua_tonumber(L, 2);
+    
+    if (!amount)
+        return 0;
+
+    /*
+    if (se->do_bufferout)
+        return _exec_read_with_bufferout(L, se, amount);
+    */
+
+    _exec_assert_bs(se, amount);
+
+    r = read(se->pout[0], (void*)se->bufferout, amount);
+    lua_pushnumber(L, r);
+    if (r <= 0)
+        return 1;
+
+    lua_pushlstring(L, se->bufferout, r);
+    return 2;
+}
+
+static int
+_f_put(lua_State *L) {
+    struct s_exec *se = _get_exec(L);
+    const char *data;
+    size_t datalen = 0, w;
+    size_t offset=0, len=(size_t)-1;
+
+    if (!lua_isstring(L, 2))
+        return luaL_error(L, "Second argument must be a string");
+
+    if (lua_isnumber(L, 3))
+        offset = (size_t)lua_tonumber(L, 3);
+    if (lua_isnumber(L, 4))
+        len = (size_t)lua_tonumber(L, 4);
+
+    data = lua_tolstring(L, 2, &datalen);
+    if (offset > datalen)
+        return luaL_error(L, "Invalid offset");
+
+    if (len == (size_t)-1)
+        len = datalen;
+    if (len + offset > datalen)
+        len = datalen - offset;
+
+    w = write(se->pin[1], data+(ptrdiff_t)offset, len);
+    
+    lua_pushnumber(L, (lua_Number)w);
+    return 1;
+}
+
+static int
+_f_get_pipesize(lua_State *L) {
+    struct s_exec *se = _get_exec(L);
+    int s_in, s_out, s_err;
+    s_in = fcntl(se->pin[1], 1032); // Linux Kernel >2.6 only, 1032==F_GETPIPE_SZ
+    s_out = fcntl(se->pout[0], 1032);
+    s_err = fcntl(se->perr[0], 1032);
+    lua_pushnumber(L, (lua_Number)s_in);
+    lua_pushnumber(L, (lua_Number)s_out);
+    lua_pushnumber(L, (lua_Number)s_err);
+    return 3;
+}
+
+static int
+_f_waitend(lua_State *L) {
+    struct s_exec *se = _get_exec(L);
+    int status = 0;
+    if (!se->pid)
+        return 0;
+
+    waitpid(se->pid, &status, 0);
+    se->pid = 0;
+    if (!(WIFEXITED(status)))
+        return 0;
+    lua_pushnumber(L, (lua_Number)(WEXITSTATUS(status)));
+    return 1;
+}
+
+
+static int
+_f_close(lua_State *L) {
+    struct s_exec *se = _get_exec(L);
+    if (!se->pin[1])
+        return luaL_error(L, "You may not call close() twice");
+
+    close(se->pin[1]);
+    close(se->pout[0]);
+    close(se->perr[0]);
+    if (se->bufferout)
+        free(se->bufferout);
+    memset(se + (ptrdiff_t)sizeof(pid_t), 0, sizeof(struct s_exec) - sizeof(pid_t));
+    return 0;
+}
+
+/*static int
+exec_fin_bufferout
+*/
+
+/*
+static int
+_f_set_bufferout(lua_State *L) {
+    struct s_exec *se = _get_exec(L);
+    size_t bosize = (size_t)luaL_checknumber(L, 2);
+
+    if (!bosize) {
+        exec_fin_bufferout(se);
+    }
+
+    se->do_bufferout = 1;
+    return 0;
+}
+*/
+
+static void
+exec_initmetatable(lua_State *L) {
+    const luaL_Reg lr[] = {
+        {"out", _f_read_out},
+        // {"bufferout", _f_set_bufferout},
+        {"put", _f_put},
+        {"close", _f_close},
+        {"waitend", _f_waitend},
+        {"getpipesize", _f_get_pipesize},
+        {0, 0}
+    };
+    lua_pushstring(L, "__index");
+    luaL_newmetatable(L, OSLAYR_EXECTYPE);
+    luaL_register(L, 0, lr);
+    lua_rawset(L, -1);
+}
+
+// no resources are freed here on failure. hmmmm... so wrong that is
+static int
+exec_openprocess(const char *exname, const char**cmds, struct s_exec *se) {
+    volatile char *erflag;
+    char dummy;
+    int pe[2];
+
+    if (pipe(se->pin) || pipe(se->pout) || pipe(se->perr))
+        return -1;
+
+    if (pipe(pe) || fcntl(pe[1], F_SETFD, (int)FD_CLOEXEC))
+        return -1;
+
+    erflag = (volatile char*)mmap(0, 1, PROT_WRITE | PROT_READ,
+        MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (!erflag)
+        return -1;
+
+    *erflag = 0;
+
+    se->pid = fork();
+    if (se->pid == -1)
+        return -1;
+        
+    if (!se->pid) {
+        char *const *ncmds = (char*const *)cmds; // it's a copy, it's another process.
+        close(pe[0]);
+        dup2(se->pin[0], 0);
+        dup2(se->pout[1], 1);
+        dup2(se->perr[1], 2);
+        close(se->pin[1]);
+        close(se->pout[0]);
+        close(se->perr[0]);
+        execvp(exname, ncmds);
+        *erflag = 1;
+        close(se->pin[0]);
+        close(se->pout[1]);
+        close(se->perr[1]);
+        close(pe[1]);
+        exit(1);
+    }
+    close(pe[1]);
+    close(se->pin[0]);
+    close(se->pout[1]);
+    close(se->perr[1]);
+    if (0 != read(pe[0], &dummy, 1)) {
+        // wtf has happened we reached here?
+        kill(se->pid, SIGTERM);
+        return -1;
+    }
+    close(pe[0]);
+    if (*erflag) {
+        // execve has failed, free everything and quit.
+        close(se->pin[1]);
+        close(se->pout[0]);
+        close(se->perr[0]);
+        munmap((void*)erflag, 1);
+        return 1;
+    }
+
+    // ok, we are up and running
+    munmap((void*)erflag, 1);
+    return 0;
+}
+
+static int
+_f_exec(lua_State *L) {
+    const char *exec_name;
+    const char *cmds[128];
+    int i, rv;
+    struct s_exec *se;
+    exec_name = luaL_checkstring(L, 1);
+    if (!lua_istable(L, 2))
+        return luaL_error(L, "Second argument must be a table (may not be nil)");
+
+    for (i = 0;; ++i) {
+        lua_pushnumber(L, i+1);
+        lua_gettable(L, 2);
+        if (!lua_isstring(L, -1))
+            break;
+        cmds[i] = lua_tostring(L, -1);
+        lua_pop(L, 1);
+    }
+    cmds[i] = 0;
+
+    se = (struct s_exec*)lua_newuserdata(L, sizeof(struct s_exec));
+    rv = se ? exec_openprocess(exec_name, cmds, se) : -1;
+    if (rv != 0) {
+        lua_pushnumber(L, rv);
+    } else {
+        luaL_newmetatable(L, OSLAYR_EXECTYPE);
+        lua_setmetatable(L, -2);
+        se->bufferout = 0;
+        se->bosize = 0;
+        // se->boused = 0;
+        // se->do_bufferout = 0;
+        // se->bo_mx = 0;
+    }
+
+    return 1;
+}
+
+static int
+_f_exec_n_pipe(lua_State *L) {
     int i, pa[2];
     pid_t childpid;
     size_t re;
@@ -79,10 +362,12 @@ int _f_exec_n_pipe(lua_State *L) {
 
 static const luaL_Reg g_lreg[] = {
     { "exec_n_pipe", _f_exec_n_pipe },
+    { "exec", _f_exec },
     {0, 0},
 };
 
 int luaopen_oslayr(lua_State *L) {
+    exec_initmetatable(L);
     luaL_register(L, "oslayr", g_lreg);
     return 1;
 }
